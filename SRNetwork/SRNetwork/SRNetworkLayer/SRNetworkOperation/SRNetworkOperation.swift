3ee42c2027b365c3eabb00d56e77d806c2898c25
Subhr Roy <subhra13heritage@gmail.com>
2018-08-06 23:39:45 +0530

    Operation Class

---
+++ SRNetwork/SRNetwork/SRNetworkLayer/SRNetworkOperation/SRNetworkOperation.swift
@@ -0,0 +1,82 @@
+//
+//  SROperationTask.swift
+//  SRNetwork
+//
+//  Created by Subhr Roy on 06/08/18.
+//  Copyright © 2018 Subhr Roy. All rights reserved.
+//
+
+import Foundation
+
+class SRNetworkOperation : Operation {
+	
+	public var  sessionTask : URLSessionTask?
+	
+	override var isAsynchronous: Bool  {return true}
+	
+	fileprivate var _executing = false
+	override var isExecuting: Bool{
+		get{
+			return _executing
+		}
+		set{
+			if _executing != newValue{
+				self.willChangeValue(forKey: "isExecuting")
+				_executing = newValue
+				self.didChangeValue(forKey: "isExecuting")
+			}
+		}
+	}
+	
+	fileprivate var _finished : Bool = false
+	override var isFinished: Bool{
+		get{
+			return _finished
+		}
+		set{
+			if _finished != newValue{
+				self.willChangeValue(forKey: "isFinished")
+				_finished = newValue
+				self.didChangeValue(forKey: "isFinished")
+			}
+		}
+	}
+	
+	func cancelOperation() -> Void{
+		self.sessionTask?.cancel()
+		self.isExecuting = false
+		self.isFinished = true
+	}
+	
+	func suspendOperation() -> Void{
+		self.sessionTask?.suspend()
+		self.isExecuting = false
+		self.isFinished = true
+	}
+	
+	func completeOpeartion() -> Void{
+		if self.isExecuting{
+			self.isExecuting = false
+			self.isFinished = true
+		}
+	}
+	
+	func resumeOperation() -> Void {
+		self.isExecuting = true;
+		sessionTask?.resume()
+	}
+	
+	override func start() {
+		if isCancelled{
+			self.isFinished = true
+			return
+		}
+		self.isExecuting = true
+		main()
+	}
+	
+	override func main() {
+		self.resumeOperation()
+	}
+	
+}
