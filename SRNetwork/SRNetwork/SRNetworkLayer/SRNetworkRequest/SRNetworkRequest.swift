710f6b5cb51653cf0b1d89bc0ae21fedc2a0c667
Subhr Roy <subhra13heritage@gmail.com>
2018-08-07 17:56:52 +0530

    Create data task class and modify

---
+++ SRNetwork/SRNetwork/SRNetworkLayer/SRNetworkRequest/SRNetworkRequest.swift
@@ -0,0 +1,69 @@
+//
+//  SRNetworkRequest.swift
+//  SRNetwork
+//
+//  Created by Subhr Roy on 07/08/18.
+//  Copyright © 2018 Subhr Roy. All rights reserved.
+//
+
+import Foundation
+
+struct SRNetworkRequest {
+	
+	func buildRequest(baseURL : URL, path : String, httpMethod : HTTPMethod, task : HTTPTask) throws -> URLRequest {
+		
+		var request = URLRequest(url: baseURL.appendingPathComponent(path),
+								 cachePolicy: .reloadIgnoringLocalAndRemoteCacheData,
+								 timeoutInterval: 10.0)
+		
+		request.httpMethod = httpMethod.rawValue
+		do {
+			switch task {
+				case .request:
+					request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+				case .requestParameters(let bodyParameters,
+										let bodyEncoding,
+										let urlParameters):
+					
+					try self.configureParameters(bodyParameters: bodyParameters,
+												 bodyEncoding: bodyEncoding,
+												 urlParameters: urlParameters,
+												 request: &request)
+				
+				case .requestParametersAndHeaders(let bodyParameters,
+												  let bodyEncoding,
+												  let urlParameters,
+												  let additionalHeaders):
+					
+					self.addAdditionalHeaders(additionalHeaders, request: &request)
+					try self.configureParameters(bodyParameters: bodyParameters,
+												 bodyEncoding: bodyEncoding,
+												 urlParameters: urlParameters,
+												 request: &request)
+			}
+			return request
+		} catch {
+			throw error
+		}
+	}
+	
+	fileprivate func configureParameters(bodyParameters: Parameters?,
+										 bodyEncoding: ParameterEncoding,
+										 urlParameters: Parameters?,
+										 request: inout URLRequest) throws {
+		do {
+			try bodyEncoding.encode(urlRequest: &request,
+									bodyParameters: bodyParameters, urlParameters: urlParameters)
+		} catch {
+			throw error
+		}
+	}
+	
+	fileprivate func addAdditionalHeaders(_ additionalHeaders: HTTPHeaders?, request: inout URLRequest) {
+		guard let headers = additionalHeaders else { return }
+		for (key, value) in headers {
+			request.setValue(value, forHTTPHeaderField: key)
+		}
+	}
+	
+}
--- SRNetwork/SRNetwork/SRNetworkLayer/SRNetworkTask/SRNetworkTask.swift
+++ SRNetwork/SRNetwork/SRNetworkLayer/SRNetworkTask/SRNetworkTask.swift
@@ -15,17 +15,53 @@ enum NetworkTaskStatus<T>{
 	case fail
 }
 
-class SRNetworkTask: SRNetworkOperation {
+class SRNetworkTask: SRNetworkOperation,SRNetworkManagerProtocol {
 	
-	convenience   init( method : HTTPMethod, baseURL : String , urlPath : String, urlHeaders : [String : Any] , parameters : [String : Any] ) {
+	internal  var method : HTTPMethod = .get
+	private  var environmentBaseUrl : URL?
+	private  var urlpath : String?
+	private  var headers : [String : Any]?
+	private  var httpBodyParam : [String : Any]?
+	private  var encodeType : ParameterEncoding = .urlEncoding
+	private var  urlParam : [String : Any]?
+	private var taskType : NetworkTaskType = NetworkTaskType.dataTask
+	
+	convenience   init( method : HTTPMethod, baseURL : URL , urlPath : String, encoding : ParameterEncoding, urlHeaders : [String : Any]? = nil , parameters : [String : Any]? = nil,urlParameter : [String : Any]? = nil, _taskType : NetworkTaskType.dataTask) {
 		
 		let taskIdentifier : String = SRNetworkConstant.uniqueTaskIdentifier()
 		self.init(taskIdentifier)
+		super.init(_taskIdentifier: taskIdentifier)
+		
+		self.httpMethod = method
+		self.environmentBaseUrl = baseURL
+		self.urlpath = urlPath
+		self.headers = urlHeaders
+		self.httpBodyParam = parameters
+		self.encodeType = encoding
+		self.urlParam = urlParameter
+		self.taskType = _taskType
 	}
 	
 	private init( _ taskIdentifier  : String) {
 		
-		super.init(_taskIdentifier: taskIdentifier)
+		
+	}
+	
+	override  func start() {
+		super.start()
+		let shared : SRNetworkManager = SRNetworkManager.sharedManager
+		shared.networkTaskDelegate = self
+		shared.networkTaskType = self.taskType
+		shared.dataTaskRequest =  self.createRequest()
+		do{
+			shared.start()
+		}catch Result.failure(NetworkResponse.badRequest){
+			
+			print("\(NetworkResponse.badRequest)")
+			
+		}catch{
+			
+		}
 	}
 
 	
@@ -33,6 +69,90 @@ class SRNetworkTask: SRNetworkOperation {
 
 extension  SRNetworkTask : EndPointType{
 	
+	var baseURL: URL {
+		return  self.environmentBaseUrl
+	}
+	
+	var path: String {
+		return  self.urlpath
+	}
+	
+	var httpMethod: HTTPMethod {
+		return  self.method
+	}
+	
+	
+	var task: HTTPTask {
+		
+		switch encodeType {
+			case .urlEncoding:
+				
+				if let header = self.headers {
+					
+					return .requestParametersAndHeaders(bodyParameters: nil, bodyEncoding: .urlEncoding, urlParameters: self.urlParam, additionHeaders: header as! HTTPHeaders)
+				}else{
+					
+					return .requestParameters(bodyParameters: nil, bodyEncoding: .urlEncoding, urlParameters: self.urlParam)
+				}
+				
+			case .jsonEncoding:
+				
+				if let header = self.headers {
+					
+					return .requestParametersAndHeaders(bodyParameters: self.httpBodyParam, bodyEncoding: .jsonEncoding, urlParameters: nil, additionHeaders: header as! HTTPHeaders)
+					
+				}else{
+					
+				   return .requestParameters(bodyParameters: self.httpBodyParam, bodyEncoding: .jsonEncoding, urlParameters: nil)
+			    }
+			
+			default:
+				if let header = self.headers {
+					
+					return .requestParametersAndHeaders(bodyParameters: nil, bodyEncoding: .urlEncoding, urlParameters: self.urlParam, additionHeaders: header as! HTTPHeaders)
+				}else{
+					
+					return .requestParameters(bodyParameters: nil, bodyEncoding: .urlEncoding, urlParameters: self.urlParam)
+			}
+			
+		}
+		
+	}
+	
+	func createRequest() -> URLRequest?{
+		
+		do{
+			
+			let request =  try SRNetworkRequest().buildRequest(baseURL: self.environmentBaseUrl, path: self.urlpath, httpMethod: self.httpMethod, task: task)
+
+				return  request
+		}catch{
+			return nil
+		}
+	}
 	
 }
 
+extension  SRNetworkTask {
+	
+	func taskDidStart(task : URLSessionTask, taskStatus : NetworkTaskStatus<Int64>, result : Result<String>){
+		
+		
+	}
+	
+	func taskDidFinish(task : URLSessionTask, taskStatus : NetworkTaskStatus<Int64>, data : Any, result : Result<String>){
+		
+		
+	}
+	
+	func taskDidFail(task : URLSessionTask, taskStatus : NetworkTaskStatus<Int64>, error : Result<String>){
+		
+		
+	}
+	
+	func taskDidWriteData(task : URLSessionTask, taskStatus : NetworkTaskStatus<Int64>,  progress : Float){
+		
+		
+	}
+	
+}
